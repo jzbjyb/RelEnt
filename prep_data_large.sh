f1b0dcbb6e3a5e16e5a6b3b48dc5b2082147da6f
jzb <zhengbaj@cs.cmu.edu>
2019-06-02 22:44:56 -0400

    add large by_entail data

--- prep_data_large.sh
+++ prep_data_large.sh
@@ -4,8 +4,8 @@
 #SBATCH --output=slurm_out/slurm-%j.out
 set -e
 
-data_dir=data/analogy_dataset/by_entail_n_way_622_subgraph50_sample1000/
-data_format=nway
+data_dir=data/analogy_dataset/by_entail_622_subgraph10_sample100_maxoccperprop10k/
+data_format=pointwise
 subgraph_file=data/subgraph/property_occurrence_all_shuf_top100k.subgraph
 
 mkdir -p ${data_dir}
@@ -16,11 +16,11 @@ python prep_data.py \
     --subgraph_file $subgraph_file \
     --emb_file ~/tir1/data/wikidata/wikidata_translation_v1.tsv.id.QP \
     --out_dir ${data_dir} \
-    --method by_entail-n_way \
+    --method by_entail \
     --train_dev_test 0.6:0.2:0.2 \
-    --max_occ_per_prop 50000 \
-    --num_occ_per_subgraph 50 \
-    --num_sample 1000 \
+    --max_occ_per_prop 10000 \
+    --num_occ_per_subgraph 10 \
+    --num_sample 100 \
     --contain_train
 
 python train.py \
@@ -30,4 +30,4 @@ python train.py \
     --subprop_file data/subprops.txt \
     --emb_file ~/tir1/data/wikidata/wikidata_translation_v1.tsv \
     --no_cuda \
-    --filter_emb data/emb/test2.emb
+    --filter_emb data/emb/test.emb
